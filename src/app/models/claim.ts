ab118391cde74b968ac054f5797e343d427365ca
Narayan <narayanareddy.muthyala@monster.com>
2018-12-08 15:16:25 +0800

    claim mode
    claim mode creation.

---
+++ src/app/models/claim.ts
@@ -0,0 +1,18 @@
+export class Claim {
+    name: string;
+    branchCodeOffice: string;
+    date: string;
+    bankBranchCode: string;
+    bankAcNumber: number;
+    nameInAC: string;
+    dateOfTransaction: string;
+    costCenter: string;
+    glCode: string;
+    description: string;
+    currency: string;
+    amount: string;
+    gst: string;
+    exchangeRate: string;
+    totalAmout: string;
+    
+}
