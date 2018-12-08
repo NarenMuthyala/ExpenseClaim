a3c07a4315270b024055d89e622cf115e916d31b
Narayan <narayanareddy.muthyala@monster.com>
2018-12-08 14:03:29 +0800

    ui fixes
    ui fixes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { EClaimComponent } from './e-claim/e-claim.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'eclaim', pathMatch: 'full'},
+  {path: 'eclaim', component: EClaimComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
